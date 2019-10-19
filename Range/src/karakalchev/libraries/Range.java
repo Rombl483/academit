9154259dc553dc64fa1fc7b330509ea980996f27
Karakalchev Roman <rombl4_83@mail.ru>
2019-10-19 18:02:27 +0700

    В модуле Range изменена иерархия пакетов. Класс Range добавлены методы пересечения, объединения и разности интервалов. Класс Main в меню добавлены команды для работы с новыми методами класса Range.

--- Range/src/karakalchev/libraries/Range.java
+++ Range/src/karakalchev/libraries/Range.java
@@ -34,6 +34,48 @@ public class Range {
     }
 
     public void print() {
-        System.out.printf("Числовой диапазон от %.2f до %.2f%n", from, to);
+        System.out.printf("Числовой диапазон =  [%.2f .. %.2f]%n", from, to);
+    }
+
+    public static Range getIntersection(Range range1, Range range2) {
+        if (range1.from <= range2.from && range2.from <= range1.to) {
+            if (range1.to <= range2.to) {
+                return new Range(range2.from, range1.to);
+            } else {
+                return new Range(range2.from, range2.to);
+            }
+        }
+
+        return null;
+    }
+
+    public static Range[] getUnion(Range range1, Range range2) {
+        Range[] ranges = new Range[2];
+
+        if (range1.from <= range2.from && range2.from <= range1.to) {
+            if (range1.to <= range2.to) {
+                ranges[0] = new Range(range1.from, range2.to);
+            } else {
+                ranges[0] = new Range(range1.from, range1.to);
+            }
+        } else {
+            ranges[0] = range1;
+            ranges[1] = range2;
+        }
+
+        return ranges;
+    }
+
+    public static Range[] getDifference(Range range1, Range range2) {
+        Range[] ranges = new Range[2];
+
+        if (range1.from <= range2.from && range2.from <= range1.to && range1.to <= range2.to) {
+            ranges[0] = new Range(range1.from, range2.from);
+        } else {
+            ranges[0] = range1;
+            ranges[1] = range2;
+        }
+
+        return ranges;
     }
 }
--- Range/src/karakalchev/program/Main.java
+++ Range/src/karakalchev/program/Main.java
@@ -1,27 +1,46 @@
 package karakalchev.program;
 
 import java.util.Scanner;
+
 import karakalchev.libraries.Range;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите начальное значение числового диапазона: ");
+        System.out.print("Введите начальное значение числового диапазона №1: ");
         double startValue = scanner.nextDouble();
 
-        System.out.print("Введите конечное занчение числового диапазона: ");
+        System.out.print("Введите конечное занчение числового диапазона №1: ");
         double endValue = scanner.nextDouble();
 
-        Range range = new Range(startValue, endValue);
+        Range range1 = new Range(startValue, endValue);
 
-        range.print();
+        System.out.print("Введите начальное значение числового диапазона №2: ");
+        startValue = scanner.nextDouble();
+
+        System.out.print("Введите конечное занчение числового диапазона №2: ");
+        endValue = scanner.nextDouble();
+
+        Range range2 = new Range(startValue, endValue);
+
+        range1.print();
+        range2.print();
         System.out.println();
 
         boolean breakIteration = false;
 
         while (!breakIteration) {
-            System.out.println("Ввведите код команды: 0-выход; 1-печать диапазона; 2,3 - изменение границ диапазона, 4 - проверить число; 5 - длина диапазона.");
+            System.out.printf("Ввведите код команды:%n" +
+                    "0-выход;%n" +
+                    "1-печать диапазона; " +
+                    "2,3 - изменение границ диапазона; " +
+                    "4 - проверить число; " +
+                    "5 - длина диапазона;%n" +
+                    "6 - пересечение интервалов; " +
+                    "7 - объединение интервалов; " +
+                    "8 - разность интервалов.%n");
+
             int commandCode = scanner.nextInt();
 
             System.out.println();
@@ -31,25 +50,25 @@ public class Main {
                     breakIteration = true;
                     break;
                 case 1:
-                    range.print();
+                    range1.print();
                     break;
                 case 2:
                     System.out.print("Введите начальное значение числового диапазона: ");
                     startValue = scanner.nextDouble();
 
-                    range.setFrom(startValue);
+                    range1.setFrom(startValue);
                     break;
                 case 3:
                     System.out.print("Введите конечное значение числового диапазона: ");
                     endValue = scanner.nextDouble();
 
-                    range.setTo(endValue);
+                    range1.setTo(endValue);
                     break;
                 case 4:
                     System.out.print("Введите число для проверки принадлежности к заданному диапазону: ");
                     double value = scanner.nextDouble();
 
-                    if (range.isInside(value)) {
+                    if (range1.isInside(value)) {
                         System.out.println("Число принадлежит заданному диапазону.");
                     } else {
                         System.out.println("Число не принадлежит заданному диапазону.");
@@ -57,7 +76,41 @@ public class Main {
 
                     break;
                 case 5:
-                    System.out.printf("Длина числового диапазона составляет %.3f%n", range.getLength());
+                    System.out.printf("Длина числового диапазона составляет %.3f%n", range1.getLength());
+                    break;
+                case 6:
+                    Range range3 = Range.getIntersection(range1, range2);
+
+                    if (range3 != null) {
+                        System.out.printf("Пересечение интервалов = [%.2f .. %.2f]%n", range3.getFrom(), range3.getTo());
+                    } else {
+                        System.out.println("Пересечение интервалов нет!");
+                    }
+
+                    break;
+                case 7:
+                    Range[] rangesUnion = Range.getUnion(range1, range2);
+
+                    System.out.println("Объединение интервалов: ");
+
+                    for (Range e : rangesUnion) {
+                        if (e != null) {
+                            System.out.printf("[%.2f .. %.2f]%n", e.getFrom(), e.getTo());
+                        }
+                    }
+
+                    break;
+                case 8:
+                    Range[] rangesDifference = Range.getDifference(range1, range2);
+
+                    System.out.println("Разность интервалов: ");
+
+                    for (Range e : rangesDifference) {
+                        if (e != null) {
+                            System.out.printf("[%.2f .. %.2f]%n", e.getFrom(), e.getTo());
+                        }
+                    }
+
                     break;
                 default:
                     System.out.printf("Код операции с номером %d не поддерживается%n", commandCode);
