40e7bbd9733451d261535d71c1203030b552ecff
Karakalchev Roman <rombl4_83@mail.ru>
2020-04-04 18:14:30 +0700

    Реализация задачи конвертация из CSV в HTML

--- ParserCSV/src/karakalchev/parsercsv/program/ParserCSV.java
+++ ParserCSV/src/karakalchev/parsercsv/program/ParserCSV.java
@@ -30,6 +30,79 @@ public class ParserCSV {
         }
     }
 
+    private static int getDelimiterStrIndex(String str, int indexFrom, String strDelimiter) {
+        String subStrDoubleQuotes = "\"\"";
+        String subStrQuotes = "\"";
+        int delimiterStrIndex = str.indexOf(strDelimiter, indexFrom);
+
+        if (strDelimiter.equals("\",")) {
+            delimiterStrIndex = str.indexOf(subStrQuotes, indexFrom);
+            int doubleQuotesIndex = str.indexOf(subStrDoubleQuotes, indexFrom);
+
+            while (doubleQuotesIndex >= 0 && doubleQuotesIndex == delimiterStrIndex) {
+                delimiterStrIndex = str.indexOf(subStrQuotes, doubleQuotesIndex + 2);
+                doubleQuotesIndex = str.indexOf(subStrDoubleQuotes, doubleQuotesIndex + 2);
+            }
+        }
+
+        return delimiterStrIndex;
+    }
+
+    private static boolean makeCells(StringBuilder contentHTML, boolean tdIsOpen, String row) {
+        int rowLength = row.length();
+        int subStrBeginIndex = 0;
+
+        while (subStrBeginIndex <= rowLength) {
+            if (!tdIsOpen) {
+                contentHTML.append("            <td>\n");
+            }
+
+            String rowDelimiter = ",";
+            int subStrEndIndex = rowLength;
+
+            if (tdIsOpen || (row.indexOf("\"", subStrBeginIndex) == subStrBeginIndex)) {
+                if (!tdIsOpen) {
+                    subStrBeginIndex = subStrBeginIndex + 1;
+                }
+
+                tdIsOpen = true;
+                rowDelimiter = "\",";
+            }
+
+            int delimiterIndex = getDelimiterStrIndex(row, subStrBeginIndex, rowDelimiter);
+
+            if (delimiterIndex >= 0 && delimiterIndex < rowLength) {
+                subStrEndIndex = delimiterIndex;
+                tdIsOpen = false;
+            }
+
+            String cellText = row.substring(subStrBeginIndex, subStrEndIndex);
+            subStrBeginIndex = subStrEndIndex + rowDelimiter.length();
+
+            contentHTML.append("                ");
+
+            cellText = cellText
+                    .replaceAll("\"\"", "\"")
+                    .replaceAll("&", "&amp")
+                    .replaceAll("<", "&lt")
+                    .replaceAll(">", "&gt");
+
+            contentHTML.append(cellText);
+
+            if (tdIsOpen) {
+                contentHTML.append("<br>");
+            }
+
+            contentHTML.append("\n");
+
+            if (!tdIsOpen) {
+                contentHTML.append("            </td>\n");
+            }
+        }
+
+        return tdIsOpen;
+    }
+
     private static StringBuilder convertFromCSVToHTML(StringBuilder contentCSV) {
         StringBuilder contentHTML = new StringBuilder();
 
@@ -38,87 +111,26 @@ public class ParserCSV {
         contentHTML.append("    <body>\n");
         contentHTML.append("        <table cols = \"5\" alignt = \"center\" border = \"1%\">\n");
 
-        for (String rows : contentCSV.toString().split("\\n")) {
-            contentHTML.append("        <tr align = \"center\">\n");
-
-            for (String columns : rows.split(",")) {
-                contentHTML.append("            <td>\n");
-                contentHTML.append("                ");
-                contentHTML.append(columns);
-                contentHTML.append("\n");
-                contentHTML.append("            </td>\n");
-            }
-
-            contentHTML.append("        </tr>\n");
-        }
-
-        contentHTML.append("        </table>\n");
-        contentHTML.append("    </body>\n");
-        contentHTML.append("</html>");
-
-        return contentHTML;
-    }
-
-/*
-    private static StringBuilder convertFromCSVToHTML(StringBuilder contentCSV){
-        StringBuilder contentHTML = new StringBuilder();
-        boolean trIsOpen = false;
         boolean tdIsOpen = false;
 
-        contentHTML.append("<html>\n");
-        contentHTML.append("    <head> Конвертация CSV в HTML </head>\n");
-        contentHTML.append("    <body>\n");
-        contentHTML.append("        <table cols = \"5\" alignt = \"center\" border = \"1%\">\n");
-
-        for (String rows : contentCSV.toString().split("\\n")) {
-            if (!trIsOpen) {
+        for (String row : contentCSV.toString().split("\\n")) {
+            if (!tdIsOpen) {
                 contentHTML.append("        <tr align = \"center\">\n");
-                trIsOpen = true;
             }
 
-            for (String columns : rows.split(",")) {
-                if (!tdIsOpen) {
-                    contentHTML.append("            <td>\n");
-                    tdIsOpen = true;
-                }
+            tdIsOpen = makeCells(contentHTML, tdIsOpen, row);
 
-//                contentHTML.append("                "+columns.replaceAll("\"\"","\""));
-//                contentHTML.append("\n");
-//                contentHTML.append("            </td>\n");
-//                tdIsOpen = false;
-
-                if (tdIsOpen && !columns.contains("\"")) {
-                    contentHTML.append("            </td>\n");
-                    tdIsOpen = false;
-                }
-
-            }
-
-            if (trIsOpen && !tdIsOpen) {
+            if (!tdIsOpen) {
                 contentHTML.append("        </tr>\n");
-                trIsOpen = false;
             }
         }
 
         contentHTML.append("        </table>\n");
         contentHTML.append("    </body>\n");
-        contentHTML.append("</html>");
+        contentHTML.append("</html>\n");
 
         return contentHTML;
     }
-
-
-    private static String getTextInQuotes(String str) {
-        int qoutesIndex = str.indexOf("\"\"", 0);
-        while (qoutesIndex < str.length() &&  qoutesIndex >= 0) {
-            System.out.println("index = " + qoutesIndex);
-            str = str.replaceFirst("\"\"","\"");
-            System.out.println(str);
-            qoutesIndex = str.indexOf("\"\"", qoutesIndex);
-        }
-        return str;
-    }
-*/
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -127,32 +139,14 @@ public class ParserCSV {
         String fileName = scanner.nextLine();
 
         System.out.println("Загрузка CSV файла");
-//        StringBuilder contentCSV = readCSVFile("D:\\Academ_IT_School\\Java\\test.csv");
         StringBuilder contentCSV = readCSVFile(fileName);
 
         fileName = fileName.replaceAll(".csv", ".html");
 
-        System.out.println("Ковертация CSV в HTML");
+        System.out.println("Ковертация CSV в HTML:" + fileName);
         StringBuilder contentHTML = convertFromCSVToHTML(contentCSV);
 
         System.out.println("Запись в HTML файл");
         createHTMLFile(contentHTML, fileName);
-
-//        String test = "\"\"\"текст1\"\",текст2, \"\"текс3\"";
-//        String test = "\"вот они: \"\",\"";
-        //System.out.println(test.replaceAll("\"\"","\""));
-        //test = test.replaceAll("^\"|\"$","");
-/*
-        String test2 = test.replace("\"\"","\"");
-        System.out.println("test = " + test);
-        System.out.println("test2 = " + test2);
-        System.out.println("getTextInQuotes(test) = " + getTextInQuotes(test));
-        String[] stringsplit = test.split("^\"|\"$");
-        System.out.println(Arrays.toString(stringsplit));
-        System.out.println("length = "+stringsplit.length);
-        for (String e : stringsplit) {
-            System.out.println(e);
-        }
-*/
     }
 }
