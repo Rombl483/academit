c6d1de4edee1256b4a72cc79255a6b99c9b8c693
Karakalchev Roman <rombl4_83@mail.ru>
2020-05-10 03:01:41 +0700

    commit

--- TemperatureConversion/src/karakalchev/temperature_conversion/model/TemperatureMeasure.java
+++ TemperatureConversion/src/karakalchev/temperature_conversion/model/TemperatureMeasure.java
@@ -16,3 +16,4 @@ public enum TemperatureMeasure {
         return title;
     }
 }
+
--- TemperatureConversion/src/karakalchev/temperature_conversion/view/ViewTemperatureConversion.java
+++ TemperatureConversion/src/karakalchev/temperature_conversion/view/ViewTemperatureConversion.java
@@ -123,3 +123,4 @@ public class ViewTemperatureConversion {
         this.buttonConvertTo = buttonConvertTo;
     }
 }
+
